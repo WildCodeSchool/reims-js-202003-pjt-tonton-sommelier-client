bfb8822207436f7982543cf5a5239297d39684ad
jojo051 <youpi051@yahoo.fr>
2020-06-10 13:53:13 +0200

    refector(lesvins)add buton for api

--- src/components/LesVins.jsx
+++ src/components/LesVins.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function LesVins() {
+  const [boxes, setapi] = useState([]);
+
+  function handleClick() {
+    Axios.get('http://localhost:8000/boxes')
+      .then((response) => response.data)
+      .then((data) => {
+        setapi({ boxes: data });
+      });
+  }
+
   return (
-    <p>hello from les vins </p>
+    <div>
+      <p>button from wines</p>
+
+      <button type="button" className="button" onClick={() => handleClick}> play </button>
+      <p>
+        {boxes.map((box) => box.name) }
+      </p>
+    </div>
   );
 }
 
