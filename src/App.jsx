a16b1474f04614f4405bcfb45b63f3003152c10d
jojo051 <youpi051@yahoo.fr>
2020-06-10 10:36:35 +0200

    refector(LesVins)create funtion for test on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import PageScan from './components/PageScan';
+import LesVins from './components/LesVins';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <PageScan />
+        <LesVins />
       </header>
     </div>
   );
---
+++ src/components/LesVins.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LesVins() {
+  return (
+    <p>hello from les vins </p>
+  );
+}
+
+export default LesVins;
