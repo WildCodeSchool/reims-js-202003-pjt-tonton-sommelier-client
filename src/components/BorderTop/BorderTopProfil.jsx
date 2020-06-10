703cb9fce305dc58ff2cfa8c81ee1999a154569c
CzerwonyV <victor.veyrier@icloud.com>
2020-06-10 16:13:42 +0200

    feat(bordertop): add bordertopwine.jsx with icon wine

---
+++ src/components/BorderTop/BorderTopProfil.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './BorderTop.css';
+
+function BorderTopProfil() {
+  return (
+    <div className="">
+      <div className="BorderTopColor">
+        <img
+          width=""
+          src="https://user-images.githubusercontent.com/57908921/84250393-48f20800-ab0c-11ea-9827-caf291615adc.png"
+          alt="ArrowImgTP"
+          className="ArrowImgTP"
+        />
+      </div>
+      <div className="ImgCenterBorderTop">
+        <img
+          width=""
+          src="https://user-images.githubusercontent.com/57908921/84266976-a777b000-ab25-11ea-8327-29d873625bd1.png"
+          alt="CardImgTP"
+          className="CardImgTP"
+          id="ProfilImg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default BorderTopProfil;
