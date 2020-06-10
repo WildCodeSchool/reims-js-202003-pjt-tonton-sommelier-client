4d6f616d3c65288ffcc773bf031e8407b6dc1331
CzerwonyV <victor.veyrier@icloud.com>
2020-06-10 16:14:41 +0200

    feat(bordertop): add bordertopwine.jsx with icon profil

---
+++ src/components/BorderTop/BorderTopWine.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './BorderTop.css';
+
+function BorderTopWine() {
+  return (
+    <div className="">
+      <div className="BorderTopColor">
+        <img
+          width=""
+          src="https://user-images.githubusercontent.com/57908921/84250393-48f20800-ab0c-11ea-9827-caf291615adc.png"
+          alt="ArrowImgTP"
+          className="ArrowImgTP"
+        />
+      </div>
+      <div className="ImgCenterBorderTop">
+        <img
+          width=""
+          src="https://user-images.githubusercontent.com/57908921/84266962-a21a6580-ab25-11ea-9fe6-8798412e2bae.png"
+          alt="CardImgTP"
+          className="CardImgTP"
+          id="WineImg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default BorderTopWine;
