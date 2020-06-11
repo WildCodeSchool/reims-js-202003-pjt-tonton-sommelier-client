f510c804c257d1df60d49e844c9551e634b2a8ae
Ricefab02 <roger.fabrice02@gmail.com>
2020-06-11 21:56:49 +0200

    build(pagetannin.jsx): create pageTannin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
+
 import PageScan from './components/PageScan';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <PageScan />
-      </header>
-    </div>
+    <PageScan />
+
   );
 }
 
-export default App;
+
