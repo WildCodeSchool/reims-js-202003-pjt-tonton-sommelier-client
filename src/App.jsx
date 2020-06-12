56131fa2d3aff80f5b8372cefd85b92708c293b8
jojo051 <youpi051@yahoo.fr>
2020-06-12 09:35:50 +0200

    refactor(wine)testprintdatasever

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import PageScan from './components/PageScan';
 import TypeWines from './components/TypeWines';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PageScan />
         <TypeWines />
       </header>
     </div>
