1d9f4bfeeb6d78b97f917f314d93e9f1a9cabfae
jojo051 <youpi051@yahoo.fr>
2020-06-16 14:42:21 +0200

    refector(createcoffret)add form in component

--- src/components/CreateCofret/CreateCoffret.jsx
+++ src/components/CreateCofret/CreateCoffret.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Axios from 'axios';
 
 function CreateCoffret() {
   return (
-    <p>hello</p>
+    <div>
+      <form>
+        <label>
+          Nom du coffret:
+          <imput type="text" name="name" />
+        </label>
+        <label>
+          Nom de la bouteille:
+          <imput type="text" name="name" />
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+    </div>
   );
 }
 
