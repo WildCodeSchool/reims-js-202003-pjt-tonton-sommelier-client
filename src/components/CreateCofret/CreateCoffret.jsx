f822e13db1fd5248d942998bb2de8d6b05ff72c1
jojo051 <youpi051@yahoo.fr>
2020-06-16 17:11:37 +0200

    fix(createcoffret)saut de ligne

--- src/components/CreateCofret/CreateCoffret.jsx
+++ src/components/CreateCofret/CreateCoffret.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import Axios from 'axios';
+import React, { useState } from 'react';
 
 function CreateCoffret() {
+  const [value, setValue] = useState('red');
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <div>
-      <form>
-        <label>
-          Nom du coffret:
-          <imput type="text" name="name" />
-        </label>
-        <br />
-        <label>
-          Nom de la bouteille:
-          <imput type="text" name="name" />
-        </label>
-        <br />
-        <input type="submit" value="Envoyer" />
+      <form action="" method="get" className="">
+        <div className="">
+          <label htmlFor="nameOfCoffret">Nom du coffret : </label>
+          <input type="text" name="name" id="name" required />
+        </div>
+        <div className="">
+          <label htmlFor="nameOfBottle">Nom de la bouteille : </label>
+          <input type="text" name="name" id="name" required />
+        </div>
+        <div>
+          <label htmlFor="TypeOfBottle">
+            Type de la bouteille :
+            <select value={value} onChange={(event) => handleChange(event)}>
+              <option value="red"> rouge </option>
+              <option value="white"> blanc </option>
+              <option value="sparkling"> pétillant </option>
+            </select>
+          </label>
+
+        </div>
       </form>
     </div>
   );
