0f7572225f1aefd18342eb14a014ab30eb9fc549
jojo051 <youpi051@yahoo.fr>
2020-06-17 08:37:25 +0200

    refactor(CreateBoxe)add fieldset

--- src/components/CreateBoxe/CreateBoxe.jsx
+++ src/components/CreateBoxe/CreateBoxe.jsx
@@ -8,8 +8,9 @@ function CreateBoxe() {
   }
 
   return (
-    <div>
-      <form action="" method="get" className="">
+    <form action="" method="get" className="">
+      <fieldset>
+        <legend>Crée votre coffret :</legend>
         <div className="">
           <label htmlFor="nameOfBoxes">Nom du coffret : </label>
           <input type="text" name="name" id="name" required />
@@ -20,18 +21,17 @@ function CreateBoxe() {
         </div>
         <div>
           <label htmlFor="TypeOfBottle">
-            Categorie de la bouteille :
+            Catégorie de la bouteille :
             <select value={value} onChange={(event) => handleChange(event)}>
               <option value="red"> rouge </option>
               <option value="white"> blanc </option>
               <option value="sparkling"> pétillant </option>
             </select>
           </label>
-          <br />
           <input type="submit" value="Envoyer" />
         </div>
-      </form>
-    </div>
+      </fieldset>
+    </form>
   );
 }
 
