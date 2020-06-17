67f47fd1eda18bbcd08fd67ffe465973cdb0649c
Ricefab02 <roger.fabrice02@gmail.com>
2020-06-17 14:18:33 +0200

    refactor(pagescan.jsx): delete import route

--- src/components/PageScan.jsx
+++ src/components/PageScan.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import {
-  BroswerRouter as Router,
-  Switch,
-  Route,
   Link,
 } from 'react-router-dom';
 import BorderTopCard from './BorderTopCard';
@@ -10,16 +7,16 @@ import BorderTopCard from './BorderTopCard';
 function PageScan() {
   return (
     <div>
-        <BorderTopCard />
-        <h1>L&apos;Accueil</h1> 
-        <Link className="code" to="/boxes/1">
-          QR CODE
-        </Link>
-        <Link className="code" to="/boxes/1">
-          CODE NUM
-        </Link>        
+      <BorderTopCard />
+      <h1>L&apos;Accueil</h1>
+      <Link className="code" to="/boxes/1">
+        QR CODE
+      </Link>
+      <Link className="code" to="/boxes/1">
+        CODE NUM
+      </Link>
     </div>
-    
+
   );
 }
 
