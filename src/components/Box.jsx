5dea216e64381bf7d01e53a9ff137b85b1361446
CzerwonyV <victor.veyrier@icloud.com>
2020-06-17 16:51:05 +0200

    feat(box): add all the img and style it with the client mock-up

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,42 +3,83 @@ import {
   Link,
   // useParams,
 } from 'react-router-dom';
+// import HomePage from './Home';
+import barique from '../Images/barique.png';
+import raisin from '../Images/raisin.png';
+import couvert from '../Images/couvert.png';
+import france from '../Images/france.png';
+import book from '../Images/book.png';
+import oeil from '../Images/oeil.png';
+import nez from '../Images/nez.png';
+import bouche from '../Images/bouche.png';
+import tannin from '../Images/tannin.png';
 import BorderTopCard from './BorderTopCard';
+import './Box.css';
 
 function Box() {
   // const { boxId } = useParams();
   return (
     <div>
-      <Link to="/">
-        <BorderTopCard />
-      </Link>
-      <Link className="button" to="/descriptions/1">
-        Tannin
-      </Link>
-      <Link className="button" to="/descriptions/2">
-        Raisin
-      </Link>
-      <Link className="button" to="/descriptions/3">
-        Tonneau
-      </Link>
-      <Link className="button" to="/descriptions/4">
-        Diner
-      </Link>
-      <Link className="button" to="/descriptions/5">
-        Region
-      </Link>
-      <Link className="button" to="/descriptions/6">
-        Histoire
-      </Link>
-      <Link className="button1" to="/descriptions/7">
-        Goût
-      </Link>
-      <Link className="button1" to="/descriptions/8">
-        Vue
-      </Link>
-      <Link className="button1" to="/descriptions/9">
-        Nez
-      </Link>
+      {/* <Link to="/">
+        <HomePage />
+      </Link> */}
+      <BorderTopCard />
+      <h3 className="H3Box">les vins rouges</h3>
+      <div className="BtnTriangleTop">
+        <div className="BtnRaisinBariqueCouvert">
+          <div className="CercleBtn">
+            <Link className="BtnLink" to="/descriptions/1">
+              <img src={raisin} alt="Raisin" className="BtnImg" />
+            </Link>
+          </div>
+          <div className="CercleBtn">
+            <Link className="BtnLink" to="/descriptions/2">
+              <img src={barique} alt="Barique" className="BtnImg" />
+            </Link>
+          </div>
+          <div className="CercleBtn">
+            <Link className="BtnLink" to="/descriptions/3">
+              <img src={couvert} alt="Couvert" className="BtnImg" />
+            </Link>
+          </div>
+        </div>
+        <div className="BtnFranceBook">
+          <div className="CercleBtn">
+            <Link className="BtnLink" to="/descriptions/4">
+              <img src={france} alt="France" className="BtnImg" />
+            </Link>
+          </div>
+          <div className="CercleBtn">
+            <Link className="BtnLink" to="/descriptions/5">
+              <img src={book} alt="Book" className="BtnImg" />
+            </Link>
+          </div>
+        </div>
+        <div className="CercleBtn">
+          <Link className="BtnLink" to="/descriptions/6">
+            <img src={tannin} alt="Tannin" className="BtnImg" />
+          </Link>
+        </div>
+      </div>
+      <div className="BtnTriangleBottom">
+        <div className="BtnOeilNez">
+          <div className="CercleBtn ImgOeilNez">
+            <Link className="BtnLink" to="/descriptions/7">
+              <img src={oeil} alt="Oeil" className="BtnImg" />
+            </Link>
+          </div>
+          <div className="CercleBtn ImgOeilNez">
+            <Link className="BtnLink" to="/descriptions/8">
+              <img src={nez} alt="Nez" className="BtnImg" />
+            </Link>
+          </div>
+        </div>
+        <div className="CercleBtn BtnBouche">
+          <Link className="BtnLink" to="/descriptions/9">
+            <img src={bouche} alt="Bouche" className="BtnImg" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
