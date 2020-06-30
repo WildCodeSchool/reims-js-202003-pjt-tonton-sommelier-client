cfa59cf313adf48b400231d2529e621515684a8c
CzerwonyV <victor.veyrier@icloud.com>
2020-06-30 13:43:32 +0200

    feat(home): change body of home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,38 +11,43 @@ import {
   faCheck,
 } from '@fortawesome/free-solid-svg-icons';
 import BorderTopHome from './BorderTopHome';
+import NavBottom from './NavBottom';
 
 function HomePage() {
   return (
-    <div>
+    <div className="AppContent">
       <BorderTopHome />
-      <div className="ImgTontonHomeContent">
-        <img
-          width="50%"
-          src="https://user-images.githubusercontent.com/57908921/84481934-2809ee00-ac97-11ea-9261-4eddecc21ba8.png"
-          alt="TontonSommelier"
-          className="ImgTontonHome"
-        />
-      </div>
-      <h3 className="TittleHome">Quel est vôtre coffret ?</h3>
-      <div>
-        <h3 className="TittleHome">Scannez le QRcode :</h3>
-        <div className="QrCodeReaderImg">
+      <div className="HomeContent">
+        <div className="ImgTontonHomeContent">
           <img
-            width="40%"
-            src="https://static.thenounproject.com/png/1433173-200.png"
-            alt="ArrowImgTP"
-            className="ImgPhoto"
+            width="50%"
+            src="https://user-images.githubusercontent.com/57908921/84481934-2809ee00-ac97-11ea-9261-4eddecc21ba8.png"
+            alt="TontonSommelier"
+            className="ImgTontonHome"
           />
         </div>
-        <div className="InputHomeContent">
-          <h3 className="TittleHome">Ou entrez le code :</h3>
-          <InputGroup>
-            <Input placeholder="Entrez vôtre code" />
-            <InputGroupAddon addonType="append"><Button color="secondary"><FontAwesomeIcon icon={faCheck} /></Button></InputGroupAddon>
-          </InputGroup>
+        <h3 className="TittleHome">Quel est vôtre coffret ?</h3>
+        <div>
+          <h3 className="TittleHome">
+            Scannez le QRcode
+            Ou entrez le code :
+          </h3>
+          <div className="QrCodeReaderImg">
+            <img
+              src="https://static.thenounproject.com/png/1433173-200.png"
+              alt="ArrowImgTP"
+              className="ImgPhoto"
+            />
+          </div>
+          <div className="InputHomeContent">
+            <InputGroup>
+              <Input placeholder="Entrez vôtre code" />
+              <InputGroupAddon addonType="append"><Button color="secondary"><FontAwesomeIcon icon={faCheck} id="IconCheck" /></Button></InputGroupAddon>
+            </InputGroup>
+          </div>
         </div>
       </div>
+      <NavBottom />
     </div>
   );
 }
