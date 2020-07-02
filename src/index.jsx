9ae04e31b0639f26ac8168ec3f1eb8fbae538726
Ricefab02 <roger.fabrice02@gmail.com>
2020-07-02 11:12:40 +0200

    refactor(index): add redux and tokenreducer

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import tokenReducer from './redux/tokenReducer'
 
-ReactDOM.render(
+const rootReducer = combineReducers({
+  token: tokenReducer
+  // <other reducers here>
+});
+const store = createStore(rootReducer);
+
+
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root'),
 );
