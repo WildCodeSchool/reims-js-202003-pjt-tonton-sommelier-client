dc260cb38b9803c6b3bb022f0c030652d7807791
Ricefab02 <roger.fabrice02@gmail.com>
2020-07-03 10:21:23 +0200

    build(tokenreducer.jsx): add reducer and action redux

--- src/redux/tokenReducer.jsx
+++ src/redux/tokenReducer.jsx
@@ -1,22 +1,22 @@
 /* ------------------- Action -----------------------*/
-const changeToken = newToken => ({
-    type: 'CHANGE_TOKEN',
-    newToken
-})
+const changeToken = (newToken) => ({
+  type: 'CHANGE_TOKEN',
+  newToken,
+});
 
 /* ------------------- Reducer -----------------------*/
-const tokenReducer = (state= '', action) => {
-    switch (action.type) {
-        case 'CHANGE_TOKEN':
-          return {
-            ...state,
-            token: action.newToken
-          };
-        // other cases
-        default:
-          return state;
-      }
-     
-}
+const tokenReducer = (state = { token: null }, action) => {
+  switch (action.type) {
+    case 'CHANGE_TOKEN':
+      return {
+        ...state,
+        token: action.newToken,
+      };
+    // other cases
+    default:
+      return state;
+  }
+};
 
 export default tokenReducer;
+export { changeToken };
