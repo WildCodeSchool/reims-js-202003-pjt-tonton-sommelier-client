f56bf5a7262579881dd2e039937b945c1ba128fd
jojo051 <youpi051@yahoo.fr>
2020-07-08 11:54:47 +0200

    refactor(question): addrouter and component top

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Axios from 'axios';
+import { connect } from 'react-redux';
+import {
+  Link,
+} from 'react-router-dom';
+import BorderTopCard from './BorderTopCard';
 
-function Questions({ props }) {
-  const [question, setQuestion] = useState(null);
+function Questions(props) {
+  const [questions, setQuestion] = useState(null);
 
   useEffect(() => {
     Axios.get(`http://localhost:8000/categories/${props.category}/contents?type=${props.type}`)
       .then((res) => {
         setQuestion(res.data);
       });
-  }, []);
+  }, [props.type]);
 
   return (
     <>
+      <div>
+        <Link to="/boxes/1">
+          <BorderTopCard />
+        </Link>
+      </div>
       <img className="imageQuestion" src="" alt="" />
       <p>{}</p>
+      <div>
+        {questions !== null ? questions.map(question => <p>{question.content}</p>) : ''}
+      </div>
       <button className="button1" type="button"> réponse1 </button>
       <button className="button2" type="button"> réponse2 </button>
       <button className="button3" type="button"> réponse3 </button>
@@ -22,4 +35,10 @@ function Questions({ props }) {
   );
 }
 
-export default Questions;
+const mapStateToProps = (state) => ({
+  type: state.reducer.type,
+  category: state.reducer.category,
+
+});
+
+export default connect(mapStateToProps)(Questions);
