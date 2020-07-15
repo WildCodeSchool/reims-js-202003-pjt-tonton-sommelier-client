a18018a499ef95ead1cce2a06ab724bbc9a41444
CzerwonyV <victor.veyrier@icloud.com>
2020-07-15 16:41:34 +0200

    feat(bordertophome): remove logout

--- src/components/BorderTopHome.jsx
+++ src/components/BorderTopHome.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import logout from '../Images/logout.png';
 import './BorderTop.css';
 
 function BorderTopHome() {
   return (
     <div className="BorderTopHome">
-      <div className="BorderTopColor BorderTopColorLogout">
-        <img
-          width=""
-          src={logout}
-          alt="logoutimg"
-          className="LogoutImg"
-        />
-      </div>
+      <div className="BorderTopColor BorderTopColorHome" />
       <div className="ImgCenterBorderTop">
         <img
           width=""
