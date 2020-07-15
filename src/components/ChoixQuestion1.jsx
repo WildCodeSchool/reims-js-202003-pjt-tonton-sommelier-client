aeff356d6fc2f6a5014debed40bf005ada816b65
lucas <lucas-p52@hotmail.fr>
2020-07-15 08:59:24 +0200

    refactor(choixquestion1): add token verification

--- src/components/ChoixQuestion1.jsx
+++ src/components/ChoixQuestion1.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Link,
-  // useParams,
+  useHistory,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Axios from 'axios';
 
 import BorderTopCard from './BorderTopCard';
 import './DebutDeJeu.css';
@@ -18,7 +19,20 @@ import couvert from '../Images/couvert.png';
 import france from '../Images/france.png';
 import raisin from '../Images/raisin.png';
 
-function ChoixQuestionContainer({ dispatch }) {
+function ChoixQuestionContainer({ dispatch, ...props }) {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (props.token == null) {
+      history.push('/login');
+    } else {
+      Axios.get('http://localhost:8000', { headers: { Authorization: `Bearer ${props.token}` } })
+        .then((response) => response.data)
+        .catch(() => {
+          history.push('/login');
+        });
+    }
+  }, [props.token, history]);
   return (
     <div>
       <Link to="/debutjeu">
@@ -67,5 +81,8 @@ function ChoixQuestionContainer({ dispatch }) {
   );
 }
 
-const ChoixQuestion1 = connect()(ChoixQuestionContainer);
+const mapStateToProps = (state) => ({
+  token: state.reducer.token,
+});
+const ChoixQuestion1 = connect(mapStateToProps)(ChoixQuestionContainer);
 export default ChoixQuestion1;
