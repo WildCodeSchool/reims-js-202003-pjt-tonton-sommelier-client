ca7208a6ac1c979e6e3429d15c57e5c4c9cfe5e6
CzerwonyV <victor.veyrier@icloud.com>
2020-07-16 16:56:36 +0200

    feat(timer): change css of timer progress

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from 'react';
 import {
   useHistory,
 } from 'react-router-dom';
 import './timer.css';
 
-function Timer(){
+function Timer() {
   const MAX_DURATION = 30;
   const [counter, setCounter] = useState(MAX_DURATION);
   const history = useHistory();
 
   useEffect(() => {
-    if (counter > 0){
+    if (counter > 0) {
       setTimeout(() => setCounter(counter - 1), 1000);
     } else {
       history.push('/reponse');
     }
   }, [counter]);
 
-  return(
-    <progress className="timer" max={MAX_DURATION} value={counter}>{counter}%</progress>    
+  return (
+    <div className="progressBarReactStrap">
+      <progress className="Timer" max={MAX_DURATION} value={counter} />
+    </div>
   );
 }
 
