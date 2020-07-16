e070ee93e60c2df3f5811130a02011f7881570a3
brithe00 <bthellier51@gmail.com>
2020-07-16 16:23:13 +0200

    refactor(home): removing the if else in the useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,15 +28,11 @@ function HomePageContainer({ dispatch, ...props }) {
   const history = useHistory();
 
   useEffect(() => {
-    if (props.token == null) {
-      history.push('/login');
-    } else {
-      Axios.get('http://localhost:8000', { headers: { Authorization: `Bearer ${props.token}` } })
-        .then((response) => response.data)
-        .catch(() => {
-          history.push('/login');
-        });
-    }
+    Axios.get('http://localhost:8000', { headers: { Authorization: `Bearer ${props.token}` } })
+      .then((response) => response.data)
+      .catch(() => {
+        history.push('/login');
+      });
   }, [props.token, history]);
 
   return (
