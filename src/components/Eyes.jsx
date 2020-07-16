6ba24790346ca7e43c6a734cda71b4568e86f0d5
CzerwonyV <victor.veyrier@icloud.com>
2020-07-17 01:05:52 +0200

    feat(mouth): change content with client model

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -3,41 +3,50 @@ import './eyes.css';
 import {
   Link,
 } from 'react-router-dom';
-import BorderTopCard from './BorderTopCard';
+import OeilLogo from '../Images/OeilLogo.png';
+import arrowwhite from '../Images/arrowwhite.png';
+import EyesPict from '../Images/EyesPict.png';
 
 function Eyes() {
-    return(
-      <>
-        <div>
-          <Link to="/debutjeu">
-            <BorderTopCard />
-          </Link>
+  return (
+    <>
+      <Link to="/debutjeu" className="SessionNameArrow">
+        <img
+          src={arrowwhite}
+          alt="ArrowImgTP"
+          className="ArrowImgTP"
+        />
+      </Link>
+      <div className="OeilNezBoucheContent">
+        <div className="WhiteCircle">
+          <img
+            src={OeilLogo}
+            alt="OeilLogo"
+            className="OeilLogo"
+          />
         </div>
-        <section className="eyes"> 
-          <h1>1er étape: LA VUE </h1>
-            <p>Lorsque vous avez votre verre en main, penchez-le à 45° au-dessus de votre fiche de dégustation.</p>
-            <img src="" alt=""/>
-            <p>Le liquide s’étale le long de la paroi pour ne plus former qu’une fine couche vers le bord : c’est à cet endroit que nous allons observer le vin. Cet examen va nous aider à déterminer la couleur du vin mais également son intensité.</p>
-          <h2>La couleur du vin</h2>
-            <p>En observant le vin de cette manière, vous pouvez facilement percevoir différentes nuances entre les vins. Nous allons en distinguer 3 pour les vins rouges :</p>
-            <ul>
-              <li>Violette</li>
-              <li>Rubis</li>
-              <li>Tuilée</li>
-            </ul>
-            <p className ="italics">À votre tour maintenant ! Quelles couleurs arrivez-vous à identifier ?</p>
-          <h2>L’intensite du vin</h2>
-            <p>Afin de faciliter votre apprentissage, nous nous baserons sur 3 intensités :</p>
-              <ul>
-                <li>Légère</li>
-                <li>Moyenne</li>
-                <li>Prononcée</li>  
-              </ul>
-            <p>Comment caractériser celle d’un vin rouge ? Penchez votre verre au-dessus de votre fiche de dégustation. Si vous arrivez à lire le texte au travers du vin, le vin est d’une intensité <span className="bolder">légère</span>. Au contraire, si vous n’arrivez pas à distinguer les lettres, elle sera <span className="bolder">prononcée</span>. </p>
-            <p className="italics"> vous de jouer ! Vos vins sont-ils plutôt clairs ou opaques ? N’hésitez pas à les comparer entre eux.</p>
-        </section>
-      </>
-    )
+        <h4 className="OeilNezBoucheH4">LA COULEUR</h4>
+        <p className="OeilNezBoucheText">
+          Lorsque vous avez votre verre en main, penchez-le à&nbsp;
+          45° au-dessus de votre fiche de dégustation
+        </p>
+        <img
+          src={EyesPict}
+          alt="EyesPict"
+          className="EyesPict"
+        />
+        <h4 className="OeilNezBoucheH4">L’INTENSITE</h4>
+        <p className="OeilNezBoucheText">
+          Comment Penchez votre verre au-dessus de votre fiche de dégustation.&nbsp;
+          Si vous arrivez à lire le texte au travers du vin,
+          &nbsp;le vin est d’une intensité légère.&nbsp;
+          Au contraire, si vous n’arrivez pas à distinguer les lettres, elle sera prononcée.
+        </p>
+        <p className="OeilNezBoucheFooter1">Légère – Moyenne – Prononcée</p>
+        <p className="OeilNezBoucheFooter2">RETOURNEZ LES TROIS PREMIERE CARTES</p>
+      </div>
+    </>
+  );
 }
 
 export default Eyes;
